a8b736a0b87cbb27f972e33caf405d80a2e87864
janetcushing <janet.cushing@gmail.com>
2018-02-24 12:14:20 -0500

    deploying

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Serve up static assets
-// app.use(express.static("client/build"));
+app.use(express.static("client/build"));
 
 //------------------------------------------
 // connect to the controller for app routes
